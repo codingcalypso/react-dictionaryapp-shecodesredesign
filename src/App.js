88764c9a7848a7a6f5ef2bed678982650c33ca71
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2023-04-28 14:36:19 -0500

    Initial setup/Bootstrap install

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from "./shecodeslogo.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={logo} className="Shecodes-logo img-fluid" alt="SheCodes Logo" />
+    </div>
+  );
+}
