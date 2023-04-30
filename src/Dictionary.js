0c5899fd1f71e951e4d6a99ebf5c03db599fd574
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2023-04-30 16:10:12 -0500

    Trying to add SheCodes Dictionary API - having errors/trying to fix

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState({});
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/hello/${keyword}`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&apiKey=37a2a46b333cct4ffebf207fd51o0167`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -25,6 +27,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
