edae03e7acc96d3bd6dfec4754f2c60f80980c6b
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2023-05-24 19:23:40 -0500

    Fixing "cannot read properties of undefined (reading 'map') error

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,21 +5,17 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              <strong>Definition:</strong>
-              {definition.definition}
-              <br />
-              <strong>Example:</strong>
-              <em>{definition.example}</em>
-              <br />
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
-          </div>
-        );
-      })}
+      <div>
+        <p>
+          <strong>Definition:</strong>
+          {props.meaning.definition}
+          <br />
+          <strong>Example:</strong>
+          <em>{props.meaning.example}</em>
+          <br />
+          <Synonyms synonyms={props.meaning.synonyms} />
+        </p>
+      </div>
     </div>
   );
 }
