abef2d627cb5759c49739ea2cd522703e14ac804
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2023-06-16 18:51:35 -0500

    updating phonetics errors

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,5 @@
 import React from "react";
 
 export default function Phonetic(props) {
-  return (
-    <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
-      <br />
-      {props.phonetic.text}
-    </div>
-  );
+  return <div className="Phonetic">{props.phonetic}</div>;
 }
