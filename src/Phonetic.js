0cb518b8e96060d57b0faf8e5551da9d84dfc19a
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2023-06-20 20:19:48 -0500

    was missing .text in Phonetic function

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  return <div className="Phonetic">{props.phonetic}</div>;
+  return <div className="Phonetic">{props.phonetic.text}</div>;
 }
